2511f632f82cbca7e9edeab8b3fbeff86453ccc5
divya02245 <divyagohil0601@gmail.com>
2025-04-26 12:51:21 +0530

    Add files via upload

---
+++ BS/project.js
@@ -0,0 +1,6 @@
+// No specific JavaScript functionality was requested, but feel free to add any interactive features here.
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Event Management Page Loaded!");
+  });
+  
